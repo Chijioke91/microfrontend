887ac171ad9c417f97f5067b596839dfdab56f10
cjaytechguy <cjaytechguy@gmail.com>
2021-08-13 23:10:40 +0100

    initial commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require('../package.json').dependencies;
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap.js',
+      },
+      shared: deps,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig); // we added devConfig second so that it can override whatever is in commonConfig if need arises
---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const { ModuleFederationPlugin } = require('webpack').container;
+const commonConfig = require('./webpack.common');
+const deps = require('../package.json').dependencies;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Marketing': './src/bootstrap',
+      },
+      shared: deps,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
